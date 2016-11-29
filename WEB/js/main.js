d0cfe491c35658c2c9afcb0f10ef2c36ba8e8729
Juan Bonforti <juanbonforti@Juans-MacBook-Pro-2.local>
2016-11-29 12:13:53 -0300

    Electromilenio_v0.2

--- WEB/js/main.js
+++ WEB/js/main.js
@@ -5,12 +5,4 @@ $(document).ready(function(){
   $('.dropdown').on('hide.bs.dropdown', function(e){
     $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
   });
-
-  $('.info-viewer').on('click', function(e){
-    if ($('.info').is(":hidden")){
-      $('.info').stop(true, true).slideDown();
-    } else {
-      $('.info').stop(true, true).slideUp();
-    }    
-  });
 })
