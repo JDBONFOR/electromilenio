7c926a7ca6a37208968a46e12780f32157de1682
Juan Bonforti <juanbonforti@Juans-MacBook-Pro-2.local>
2016-11-30 15:25:59 -0300

    Electromilenio_v0.7

--- WEB/php/contact.php
+++ WEB/php/contact.php
@@ -4,14 +4,12 @@ ini_set('display_errors', '1');
 require_once 'phpmailer/PHPMailerAutoload.php';
 
 if (isset($_POST['firstName']) && isset($_POST['email']) && isset($_POST['phone']) && isset($_POST['message'])) {
-
   //check if any of the inputs are empty
   if (empty($_POST['firstName']) || empty($_POST['email']) || empty($_POST['message'])) {
     $data = array('success' => false, 'message' => 'Debes llenar todos los campos del formulario.');
     echo json_encode($data);
     exit;
   }
-
   //create an instance of PHPMailer
   $mail = new PHPMailer();
 
@@ -32,12 +30,11 @@ if (isset($_POST['firstName']) && isset($_POST['email']) && isset($_POST['phone'
       $data = array('success' => false, 'message' => 'El mensaje no pudo ser enviado. Error: ' . $mail->ErrorInfo);
       echo json_encode($data);
       exit;
+  } else {
+    echo "<meta http-equiv='Refresh' content='1;url=http://jdb-hosting.esy.es/electromilenio'>";
   }
-
-  $data = array('success' => true, 'message' => 'Gracias por su contacto!');
-  echo json_encode($data);
 
 } else {
   $data = array('success' => false, 'message' => 'Debes llenar todos los campos del formulario.');
-  echo json_encode($data);
+  echo json_encode($_POST['firstName'] . " " . $_POST['email'] . " " . $_POST['phone'] . " " . $_POST['message']);
 }
